74539446c4410a3d49f20496a573492cab31ace2
kekhon <elina.stam.rap@gmail.com>
2020-07-02 04:25:54 +0100

    Init commit

---
+++ src/main/java/net/ultravanilla/ultrafoods/UltraFoods.java
@@ -0,0 +1,16 @@
+package net.ultravanilla.ultrafoods;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltraFoods extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
