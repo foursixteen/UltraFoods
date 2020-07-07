4cd1199fd419630cb2c8b7ec6c74b564f309301e
Penumbra <penumbra420@gmail.com>
2020-07-07 15:01:21 -0400

    Day 1 part 2

--- src/main/java/net/ultravanilla/ultrafoods/UltraFoods.java
+++ src/main/java/net/ultravanilla/ultrafoods/UltraFoods.java
@@ -2,36 +2,44 @@ package net.ultravanilla.ultrafoods;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
-public final class UltraFoods extends JavaPlugin implements Listener
-{
+public final class UltraFoods extends JavaPlugin implements Listener {
+
+    private SkullMeta burgerMeta;
 
     @Override
     public void onEnable() {
 
         System.out.println("AAAA");
 
-        ShapedRecipe recipe = new ShapedRecipe(new ItemStack(Material.DIAMOND, 4));
+        ItemStack burger = new ItemStack(Material.PLAYER_HEAD, 1);
+
+        burgerMeta = (SkullMeta) burger.getItemMeta();
+        burgerMeta.setOwner("claivin");
+        burgerMeta.setDisplayName(ChatColor.YELLOW + "Burger.");
+
+        burger.setItemMeta(burgerMeta);
+
+        ShapedRecipe recipe = new ShapedRecipe(burger);
 
         recipe.shape(
-                "SDC",
-                "DFD",
-                "CDS"
+                "SDC"
         );
 
         recipe.setIngredient('S', Material.STICK);
         recipe.setIngredient('D', Material.DIRT);
-        recipe.setIngredient('F', Material.FLINT);
         recipe.setIngredient('C', Material.COBBLESTONE);
 
         getServer().addRecipe(recipe);
@@ -44,11 +52,50 @@ public final class UltraFoods extends JavaPlugin implements Listener
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         Action action = event.getAction();
-        if(action == Action.RIGHT_CLICK_BLOCK) {
-            Block block = event.getClickedBlock();
-            if(block.getType() == Material.DIRT) {
-                player.setHealth(0);
-                player.sendMessage(ChatColor.DARK_RED + "HOW DARE YOU TOUCH MY ENVIRONMENT!!!!");
+
+        if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
+
+            ItemStack item = player.getInventory().getItemInMainHand();
+            ItemMeta meta = item.getItemMeta();
+
+            if (meta != null && item.getType() == Material.PLAYER_HEAD) {
+
+                SkullMeta skullMeta = (SkullMeta) meta;
+
+                if (player.getFoodLevel() < 20 &&
+                        skullMeta.getOwner().equals(burgerMeta.getOwner()) &&
+                        skullMeta.getDisplayName().equals(burgerMeta.getDisplayName())) {
+
+                    event.setCancelled(true);
+
+                    player.setSprinting(false);
+                    player.setWalkSpeed(player.getWalkSpeed() / 2);
+                    item.setAmount(item.getAmount() - 1);
+
+                    BukkitScheduler scheduler1 = getServer().getScheduler();
+                    scheduler1.scheduleSyncRepeatingTask(this, new Runnable() {
+                                @Override
+                                public void run() {
+                                    player.getWorld().playSound(player.getLocation(), Sound.ENTITY_MOOSHROOM_EAT, 1.0F, 1.0F);
+                                }
+                            }, 0L, 7L
+                    );
+
+                    BukkitScheduler scheduler2 = getServer().getScheduler();
+                    scheduler2.scheduleSyncDelayedTask(this, new Runnable() {
+                        @Override
+                        public void run() {
+                            scheduler1.cancelTasks(UltraFoods.this);
+
+                            int level = player.getFoodLevel() + 7;
+                            player.setFoodLevel(level);
+                            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_PLAYER_BURP, 1.0F, 1.0F);
+                            player.sendMessage("TASTEY MAC DONALD");
+                            player.setWalkSpeed(player.getWalkSpeed() * 2);
+                        }
+                    }, 3 * 20L);
+
+                }
             }
         }
     }
